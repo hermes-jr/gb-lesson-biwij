6f1d933148ec85467319f74d7482cdd29d4f6c55
Mikhail Antonov <hermes@compot.ru>
2015-11-10 04:48:51 +0300

    Some changes to OperatorFactory

--- src/OperatorFactory.java
+++ src/OperatorFactory.java
@@ -1,5 +1,30 @@
+import java.lang.reflect.InvocationTargetException;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created on 10.11.2015.
  */
 public class OperatorFactory {
+    static Map<String, Class> ops = new HashMap<String, Class>();
+
+    static
+    {
+        ops.put("PRINT", Print.class);
+    }
+
+    public static void createOperator(String opName, String substring) {
+        Class opClass = ops.get(opName);
+        try {
+            opClass.getConstructor(String.class).newInstance(substring);
+        } catch (InstantiationException e) {
+            throw new RuntimeException(e);
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException(e);
+        } catch (InvocationTargetException e) {
+            throw new RuntimeException(e);
+        } catch (NoSuchMethodException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
